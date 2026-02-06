3f0a1a74100772aceb40622dffbead35ec5875ba
Al Zee <z@alz.ee>
2026-02-06 15:59:06 +0800

    nth

--- symfony/init.sh
+++ symfony/init.sh
@@ -8,17 +8,24 @@
 
 ############### Main Part ###############
 project=$(basename $PWD)
+passwd=111
+
+[ -f .env.local ] && . .env.local
 
 sudo -u postgres psql -c "create role $project with login createdb password '$project'";
 
 bin/console doc:data:create
 bin/console doc:m:m -n
 
-bin/console adduser --root root 111
-bin/console adduser -s al 111
-bin/console adduser -a admin 111
+bin/console adduser --root root $passwd
+bin/console adduser -s al $passwd
+bin/console adduser -a admin $passwd
 
 #bin/console lexik:jwt:generate-keypair --overwrite -n
 
-bin/console asset-map:compile
+if [ "$APP_ENV" = prod ]; then
+    bin/console asset-map:compile
+    bin/console secrets:generate-keys
+    # bin/console secrets:set APP_SECRET
+fi
 
